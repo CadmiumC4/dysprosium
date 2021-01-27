5d1fb69ad4fea9f29b68441c72500995459dacee
Yiğit Cemal Öztürk <69117901+CadmiumC48@users.noreply.github.com>
2021-01-27 13:11:16 +0300

    Create dy66_snowflake.h

---
+++ headers/dy66_snowflake.h
@@ -0,0 +1,10 @@
+union DISCORD_SNOWFLAKE
+{
+  unsigned long int value;
+  struct DISCORD_SNOWFLAKE_ELEMENTS {
+   unsigned int process_snowflake_id:12;
+   unsigned int process_id:5;
+   unsigned int worker_id:5;
+   unsigned int discord_timestamp:54;
+   } elements;
+ };
