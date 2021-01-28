67e6f9997ed6a9789beacb1685e5f9fbcea78410
Yiğit Cemal Öztürk <69117901+CadmiumC48@users.noreply.github.com>
2021-01-28 10:26:33 +0300

    Elements are updated for endians.

--- headers/dy66_snowflake.h
+++ headers/dy66_snowflake.h
@@ -5,11 +5,17 @@ namespace dy66dc
 typedef union 
 {
   unsigned long int value;
-  struct DISCORD_SNOWFLAKE_ELEMENTS {
+  struct DISCORD_SNOWFLAKE_ELEMENTS_LE {
    unsigned int process_snowflake_id:12;
    unsigned int process_id:5;
    unsigned int worker_id:5;
    unsigned int discord_timestamp:54;
-   } elements;
+   } elements_le;
+  struct DISCORD_SNOWFLAKE_ELEMENTS_BE {
+    unsigned int discord_timestamp:54;
+    unsigned int worker_id:5;
+    unsigned int process_id:5;
+    unsigned int process_snowflake_id:12;
+  } elements_be;
  } DISCORD_SNOWFLAKE;
 }
